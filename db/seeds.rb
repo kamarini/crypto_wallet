4f6962e8e733e23b584ef0fc703b8fcc7765ad87
Kátia Andréa Marini Marson <kamarini7@gmail.com>
2020-01-24 11:54:45 -0300

    Create seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 #Use o rails dev:setup (lib/tasks/dev.rake)
-# 
+#
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,3 +8,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Coin.create!(
+  description: "Bitcoin",
+  acronym: "BTC",
+  url_image: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG47.png"
+)
+
+Coin.create!(
+  description: "Ethereum",
+  acronym: "ETH",
+  url_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Ethereum_logo_2014.svg/256px-Ethereum_logo_2014.svg.png"
+)
+
+Coin.create!(
+  description: "Dash",
+  acronym: "DASH",
+  url_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQsJnpKnbWwJsEO9E7VxR6cTXgb4xjbzudPvqjVXrMsYuBfOOVK"
+)
